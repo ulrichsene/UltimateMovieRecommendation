c7107d5d2f398550ee024e6af798788ddaee5dcf
ulrichsene <evaulrichsen0@gmail.com>
2025-02-02 22:02:39 -0800

    updated node file

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,17 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// Serve static files from the correct folder
+app.use(express.static(path.join(__dirname, '../html')));
+app.use(express.static(path.join(__dirname, '../styles')));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../html/index.html'));
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
