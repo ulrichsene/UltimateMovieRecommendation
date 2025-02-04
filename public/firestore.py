c019b2a23ee3e3ff2befe93432f7279577c1a08e
lbodenbender <lbodenbender@zagmail.gonzaga.edu>
2025-02-04 14:18:42 -0800

    set up application default credentials

--- public/firestore.py
+++ public/firestore.py
@@ -1,7 +1,13 @@
 import firebase_admin
 from firebase_admin import firestore
+from firebase_admin import credentials
 
 def init_firestore_client():
     # Application Default credentials are automatically created.
-    app = firebase_admin.initialize_app()
-    db = firestore.client()
+    cred = credentials.ApplicationDefault()
+
+    firebase_admin.initialize_app(cred)
+    db = firestore.client() 
+
+if __name__ == "__main__":
+    init_firestore_client()
