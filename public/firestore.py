d369180b30b60aa1880c33cb7ab7d3330b1c4233
lbodenbender <lbodenbender@zagmail.gonzaga.edu>
2025-02-04 13:23:12 -0800

    init firestore client function

--- public/firestore.py
+++ public/firestore.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import firestore
 
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
-db = firestore.client()
+def init_firestore_client():
+    # Application Default credentials are automatically created.
+    app = firebase_admin.initialize_app()
+    db = firestore.client()
