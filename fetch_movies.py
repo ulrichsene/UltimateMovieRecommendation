5b39e5b54165095aa8656724df8d91168cc483af
Hannah Horn <hhorn@Hannahs-MacBook-Air-9.local>
2025-02-04 20:47:29 -0800

    api work

--- fetch_movies.py
+++ fetch_movies.py
@@ -1,55 +1,68 @@
 import requests
-import json
+import json # allows us to parse API responses and save results to a json file
 
+# declare the key and starting point for all api calls here (global)
 api_key = "C9jD5sQKfD7UMfht2vFXmTIdJKzZHzJkV0TmsiDn"
-api_url = f"https://api.watchmode.com/v1/list-titles/?apiKey={api_key}"
+base_url = "https://api.watchmode.com/v1"
 
-def get_all_movies():
-    response = requests.get(api_url)
+def get_movies(limit=5):
+    """This function fetches a list of movies from Watchmode API."""
+    url = f"{base_url}/list-titles/?apiKey={api_key}&limit={limit}"
+    # this url is basically built using the base url with the list-titles endpoint, the api key, and only wanted 5 to be returned (parameter)
+    response = requests.get(url) # sends get request to watchmode api to retrieve the movie data
 
-    print("Status Code:", response.status_code)
-    print("Response Text:", response.text[:500])  
+    if response.status_code == 200: # means response was successful
+        movies = response.json().get("titles", [])
+        if movies:
+            return movies  # Returns the list of movies
+    print(f"Failed to fetch movies: {response.status_code}")
+    return [] # if movies are not found, returns empty list
+
+def get_streaming_services(movie_id):
+    """This function fetches the streaming platforms for a given movie based on its movie_id."""
+    url = f"{base_url}/title/{movie_id}/sources/?apiKey={api_key}"
+    # this url is built in a similar way but passes in a specific movie id
+    response = requests.get(url)
+
     if response.status_code == 200:
-        try:
-            data = response.json()
-
-            if "titles" in data:
-                movie_titles = data["titles"]
-
-                # view a sample of the data from the API
-                print("Sample Movie Titles:", [movie["title"] for movie in movie_titles[:5]])
-
-                # saves the movie info to a json file
-                with open("all_movies.json", "w") as file:
-                    json.dump(movie_titles, file, indent=4)
-
-            else:
-                print("Unexpected response format:", data)
-
-        except json.JSONDecodeError:
-            print("Error: Response is not valid JSON")
+        data = response.json()
+        us_platforms = [ # for each platform it collects the name, price to rent/buy, direct url to watch
+            {
+                "name": service["name"], 
+                "price": service["price"],
+                "web_url": service["web_url"]
+            }
+            for service in data if service.get("region") == "US" # makes sure that it only includes platforms available in the US
+        ]
+        return us_platforms
     else:
-        print(f"Failed to fetch movies: {response.status_code} - {response.text}")
+        print(f"Failed to fetch streaming sources for {movie_id}: {response.status_code}")
+        return []
+
+def fetch_movies_with_platforms():
+    """This function fetches multiple (5 in this case) movies, retrieves their streaming platforms, and saves to a json file."""
+    movies = get_movies(limit=5)  # calls the get_movies function to fetch 5 movie -> results in a list of movie objects
+    movie_list = []
+
+    for movie in movies: # function loops through each movie in movie list and gets the id and calls the get_streaming service function to get platforms
+        movie_id = movie["id"]
+        platforms = get_streaming_services(movie_id)
+
+        movie_data = { # for each movie, creates a dictionary containing relevant information
+            "id": movie_id,
+            "title": movie["title"],
+            "platforms": platforms
+        }
+        movie_list.append(movie_data)  
+
+    # saves all movie data to a JSON file
+    with open("movies_data.json", "w") as outfile:
+        json.dump(movie_list, outfile, indent=4)
+
+    print("All movie data saved to movies_data.json")
 
 if __name__ == "__main__":
-    get_all_movies()
+    fetch_movies_with_platforms()
 
-
-# basic output
-#  {
-#   "id": 3217677,
-#   "title": "Paradise",
-#   "year": 2025,
-#   "imdb_id": "tt27444205",
-#   "tmdb_id": 245927,
-#   "tmdb_type": "tv",
-#   "type": "tv_series"
-# }
-
-# see is a list of titles available, but does not show streaming platform info (like Netflix, Hulu, etc.)
-# i think what we need to do is use the title details endpoint which has more info
-# think we can modify the script to make a second request for each movie/TV series to get streaming platform info.
-# Fetches movie titles from the list-titles endpoint.
-# For each movie, it makes a request to the title/{title_id}/details/ endpoint to fetch streaming platforms.
-# Prints out the platforms available for each movie.
-
+# note: this code just outputs one movie with the following information (found on movies_data.json)
+# id, title, platforms available, price, web_url (direct link to watch)
