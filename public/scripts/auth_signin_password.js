ee6c8fd829bbbd89404969846057cf0431d879c7
ulrichsene <evaulrichsen0@gmail.com>
2025-02-05 14:17:30 -0800

    added signin

--- public/scripts/auth_signin_password.js
+++ public/scripts/auth_signin_password.js
@@ -1,15 +1,14 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
+signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
-    // Signed up 
+    // Signed in 
     const user = userCredential.user;
     // ...
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    // ..
   });
+  
