a15d284ffcefdc551c36ac909fb1bc3e16580362
Hannah Horn <hhorn@Hannahs-MacBook-Air-9.local>
2025-02-15 11:01:34 -0800

    similarity working

--- algorithm_work.py
+++ algorithm_work.py
@@ -12,14 +12,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # read the file
 data = pd.read_csv("input_data/final_cleaned_IMDb_dataset.csv")
-print(data.head())
+# print(data.head())
 
 # extract the overview column
-if 'Overview' in data.columns:
-    print("\nOverview column found. Displaying first few overviews:\n")
-    print(data['Overview'].head(10))
-else:
-    print("\nError: 'Overview' column not found in dataset!")
+# if 'Overview' in data.columns:
+#     print("\nOverview column found. Displaying first few overviews:\n")
+#     print(data['Overview'].head(10))
+# else:
+#     print("\nError: 'Overview' column not found in dataset!")
 
 
 # helper function to start preprocessing this column
@@ -49,10 +49,10 @@ output_data = data[['movie title', 'Cleaned_Overview']]
 # output_data.to_csv('movies_with_cleaned_overview.csv', index=False)
 
 # debugging: print before/after for comparison
-print("\nOriginal vs Cleaned Overview:\n")
-for i in range(5): # just display first 5 movies
-    print(f"Original: {data['Overview'].iloc[i]}")
-    print(f"Cleaned: {output_data['Cleaned_Overview'].iloc[i]}\n")
+# print("\nOriginal vs Cleaned Overview:\n")
+# for i in range(5): # just display first 5 movies
+#     print(f"Original: {data['Overview'].iloc[i]}")
+#     print(f"Cleaned: {output_data['Cleaned_Overview'].iloc[i]}\n")
 
 # NOTE: NEED TO MAKE SURE NOT OVER PROCESSING AND PLOT SUMMARY ISN'T READABLE!
 
@@ -60,7 +60,7 @@ for i in range(5): # just display first 5 movies
 # use title, genres, overview, plot keywords, director, top 5 cast members
 
 # print a sample to verify
-print("\nSample Combined Feature Text:\n")
+# print("\nSample Combined Feature Text:\n")
 
 # Step 1: Define the function to clean the column
 def list_to_string(value):
@@ -97,20 +97,72 @@ output_data2.to_csv('movies_with_combined_features.csv', index=False)
 # apply the TF-IDF Vectorization on the merged text column.
 # each movie will now be represented as a numerical vector.
 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(output_data2["combined_features"])
 
 # this allows us to compute the similarity between different movies.
 
 # step 4: use cosine similarity to measure how similar movies are based on vectors
 # return top 3 or something? closest matches
 
-def get_similar_movies(movie_input, data, X):
-    pass
+def find_movie_by_title(movie_input):
+    # loop through the dataset to get the index and title of the inputted movie
+    for index, row in enumerate(data["movie title"]):
+        if movie_input.lower() == row.lower(): # compares the lowercase title
+            print(f"Movie: {movie_input} found at index {index}.")
+            return index
+    
+    # if movie not found
+    print(f"Movie {movie_input} not found in the dataset. Try another movie.")
+    return None
+
+known_movie = "iron man 2"
+unknown_movie = "Vampire Diaries"
+
+# test with both movies
+find_movie_by_title(known_movie)
+find_movie_by_title(unknown_movie)
+
+def get_similar_movies(movie_title, top_n = 3):
+    # call function and get the index of desired movie
+    input_movie_index = find_movie_by_title(movie_title)
+
+    if input_movie_index is None:
+        return None
+    
+    # get the vector for the input movie
+    vectorizer = TfidfVectorizer()
+    movie_vectors = vectorizer.fit_transform(output_data2)
+    input_movie_vector = movie_vectors[input_movie_index]
+
+    # calculate the cosine similarity between the input movie and all other movies
+    similarity_scores = cosine_similarity(input_movie_vector, movie_vectors).flatten()
+    # resul is a 1D array of similarity scores
+
+    # want to exclude the input movie (bc would always have highest score of 1)
+    similarity_scores[input_movie_index] = -1
+
+    # sort these similarity scores in descending order
+    sorted_indexes = similarity_scores.argsort()[::-1]
+
+    # get whatever top n we want
+    top_movies = sorted_indexes[:top_n]
+
+    # extract the movie titles that correspond to these indexes
+    similar_movies = data['movie title'].iloc[top_movies]
+    
+    # extract the similarity score of the top movies
+    similar_movies_score = similarity_scores[top_movies]
+
+    return list(zip(similar_movies, similar_movies_score))
 
 # step 5: for the actual recommendation function we could do something like this:
 # take the movie title as input from the user
 # find the movie in the dataset (if not found, return error message)
 # extract its feature vector
-# compute similarity scores between this movie and all other movies
+# cocmpute similarity scores between this movie and all other movies
 # sort the movies based on similarity scores (descending order?)
+
+
+# example
+known_movie = "Top Gun: Maverick"
+similar_movies = get_similar_movies(known_movie, top_n=3)
+print(similar_movies)
